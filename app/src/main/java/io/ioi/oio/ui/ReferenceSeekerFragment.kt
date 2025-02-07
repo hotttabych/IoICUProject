ea764317927fd22e9fae541c59ae4571d29b1e4e
hotttabych <130864666+hotttabych@users.noreply.github.com>
2025-02-07 17:18:32 +0300

    Fixing Reference Seeker

--- app/src/main/java/io/ioi/oio/ui/ReferenceSeekerFragment.kt
+++ app/src/main/java/io/ioi/oio/ui/ReferenceSeekerFragment.kt
@@ -8,13 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
-import io.ioi.oio.R
 import io.ioi.oio.databinding.FragmentReferenceSeekerBinding
 import java.util.Calendar
+import java.util.Date
 
 class ReferenceSeekerFragment : BaseFragment() {
     private var _binding: FragmentReferenceSeekerBinding? = null
     private val binding get() = _binding!!
+
+    private var startDate: Date? = null
+    private var endDate: Date? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +26,39 @@ class ReferenceSeekerFragment : BaseFragment() {
         _binding = FragmentReferenceSeekerBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            pickStartDateButton.setOnClickListener {
+                datePickerFragment { year, month, day ->
+                    Calendar.getInstance().apply {
+                        set(Calendar.YEAR, year)
+                        set(Calendar.MONTH, month)
+                        set(Calendar.DAY_OF_MONTH, day)
+                    }.also { calendar ->
+                        startDate = calendar.time
+                    }
+                }.show(parentFragmentManager, "datePicker")
+            }
+            pickEndDateButton.setOnClickListener {
+                datePickerFragment { year, month, day ->
+                    Calendar.getInstance().apply {
+                        set(Calendar.YEAR, year)
+                        set(Calendar.MONTH, month)
+                        set(Calendar.DAY_OF_MONTH, day)
+                    }.also { calendar ->
+                        endDate = calendar.time
+                    }
+                }.show(parentFragmentManager, "datePicker")
+            }
+        }
+    }
 }
 
-class DatePickerFragment(private var onDateSetCallback: () -> Unit) : DialogFragment(), DatePickerDialog.OnDateSetListener {
+class DatePickerFragment(private var onDateSetCallback: (year: Int, month: Int, day: Int) -> Unit) :
+    DialogFragment(), DatePickerDialog.OnDateSetListener {
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
@@ -36,12 +69,10 @@ class DatePickerFragment(private var onDateSetCallback: () -> Unit) : DialogFrag
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
-        onDateSetCallback.invoke()
+        onDateSetCallback.invoke(year, month, day)
     }
+}
 
-    companion object {
-        fun datePickerFragment(onDateSet: () -> Unit) {
-            DatePickerFragment(onDateSet)
-        }
-    }
+fun datePickerFragment(onDateSet: (Int, Int, Int) -> Unit): DatePickerFragment {
+    return DatePickerFragment(onDateSet)
 }
