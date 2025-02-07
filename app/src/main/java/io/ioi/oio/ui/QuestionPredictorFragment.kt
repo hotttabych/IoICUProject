7e7047e1c37ca53c1ed3f4ad04c811380f34c2e4
Stacey <nastamicura2@gmail.com>
2025-02-07 17:41:52 +0300

    Open file Quastion

--- app/src/main/java/io/ioi/oio/ui/QuestionPredictorFragment.kt
+++ app/src/main/java/io/ioi/oio/ui/QuestionPredictorFragment.kt
@@ -1,9 +1,13 @@
 package io.ioi.oio.ui
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import io.ioi.oio.R
 import io.ioi.oio.databinding.FragmentQuestionPredictorBinding
 
@@ -11,11 +15,29 @@ class QuestionPredictorFragment : BaseFragment() {
     private var _binding: FragmentQuestionPredictorBinding? = null
     private val binding get() = _binding!!
 
+    private val getFileLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            handleFileUpload(it)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentQuestionPredictorBinding.inflate(inflater, container, false)
+
+        binding.addFileButtonPdf.setOnClickListener {
+            getFileLauncher.launch("pdf/*")}
+
         return binding.root
     }
+
+    private fun handleFileUpload(uri: Uri) {
+        val filePath = uri.path
+        Toast.makeText(requireContext(), "Файл загружен: $filePath", Toast.LENGTH_SHORT).show()
+    }
 }
+
+
+
