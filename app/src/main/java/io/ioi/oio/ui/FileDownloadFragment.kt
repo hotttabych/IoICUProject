7ad7d7efe42e1a09a56bf505e829b1a20bcf9b35
hotttabych <130864666+hotttabych@users.noreply.github.com>
2025-02-07 18:24:14 +0300

    Fixing FileDownloadFragment

--- app/src/main/java/io/ioi/oio/ui/FileDownloadFragment.kt
+++ app/src/main/java/io/ioi/oio/ui/FileDownloadFragment.kt
@@ -1,14 +1,13 @@
 package io.ioi.oio.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.ioi.oio.R
 import io.ioi.oio.databinding.FragmentFileDownloadBinding
 
-class FileDownloadFragment : Fragment() {
+class FileDownloadFragment : BaseFragment() {
     private var _binding: FragmentFileDownloadBinding? = null
     private val binding get() = _binding!!
 
