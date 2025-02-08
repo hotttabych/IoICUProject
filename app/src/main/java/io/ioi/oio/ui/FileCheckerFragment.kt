eee9aa72d791afeac15118d705ca60ddae7f6f6e
Stacey <nastamicura2@gmail.com>
2025-02-08 10:28:01 +0300

    Small fix

--- app/src/main/java/io/ioi/oio/ui/FileCheckerFragment.kt
+++ app/src/main/java/io/ioi/oio/ui/FileCheckerFragment.kt
@@ -2,7 +2,6 @@ package io.ioi.oio.ui
 
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +15,12 @@ class FileCheckerFragment : BaseFragment() {
     private var _binding: FragmentFileCheckerBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var fileUri_word: Uri
+    private var fileUri_word: Uri? = null
 
     private val getFileLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             handleFileUpload(it)
-            if(fileUri_word.path?.isNotEmpty() == true){
+            if(fileUri_word?.path?.isNotEmpty() == true){
                 binding.addFileButtonWord.visibility = View.GONE
                 binding.addFileButtonWordReady.visibility = View.VISIBLE
             }
@@ -38,7 +37,8 @@ class FileCheckerFragment : BaseFragment() {
             getFileLauncher.launch("application/vnd.openxmlformats-officedocument.wordprocessingml.document")}
 
         binding.formatFileButton.setOnClickListener {
-            if (fileUri_word.path?.isNotEmpty() == true) {
+            if (fileUri_word?.path?.isNotEmpty() == true) {
+                Toast.makeText(requireContext(), "Данный функционал находится в разработке", Toast.LENGTH_SHORT).show()
                 findNavController().navigate(R.id.file_download_fragment)
             }
         }
--- app/src/main/java/io/ioi/oio/ui/QuestionPredictorFragment.kt
+++ app/src/main/java/io/ioi/oio/ui/QuestionPredictorFragment.kt
@@ -5,25 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
 import io.ioi.oio.R
 import io.ioi.oio.databinding.FragmentQuestionPredictorBinding
-import java.io.File
 
 class QuestionPredictorFragment : BaseFragment() {
     private var _binding: FragmentQuestionPredictorBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var fileUri_pdf: Uri
-    private lateinit var fileUri_word: Uri
-    private lateinit var fileUri_pptx: Uri
+    private var fileUri_pdf: Uri? = null
+    private var fileUri_word: Uri? = null
+    private var fileUri_pptx: Uri? = null
 
     private val getFileLauncher1 = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             handleFileUpload1(it)
-            if(fileUri_pdf != null){
+            if (fileUri_pdf?.path?.isNotEmpty() == true){
                 binding.addFileButtonPdf.visibility = View.GONE
                 binding.addFileButtonPdfReady.visibility = View.VISIBLE
             }
@@ -32,7 +31,7 @@ class QuestionPredictorFragment : BaseFragment() {
     private val getFileLauncher2 = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             handleFileUpload2(it)
-            if(fileUri_word != null){
+            if(fileUri_word?.path?.isNotEmpty() == true){
                 binding.addFileButtonWord.visibility = View.GONE
                 binding.addFileButtonWordReady.visibility = View.VISIBLE
             }
@@ -41,7 +40,7 @@ class QuestionPredictorFragment : BaseFragment() {
     private val getFileLauncher3 = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             handleFileUpload3(it)
-            if(fileUri_pptx != null){
+            if(fileUri_pptx?.path?.isNotEmpty() == true){
                 binding.addFileButtonPptx.visibility = View.GONE
                 binding.addFileButtonPptxReady.visibility = View.VISIBLE
             }
@@ -51,16 +50,25 @@ class QuestionPredictorFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentQuestionPredictorBinding.inflate(inflater, container, false)
 
         binding.addFileButtonPdf.setOnClickListener {
-            getFileLauncher1.launch("pdf/*")
+            getFileLauncher1.launch("application/pdf")
         }
         binding.addFileButtonWord.setOnClickListener {
-            getFileLauncher2.launch("docx/*")}
+            getFileLauncher2.launch("application/vnd.openxmlformats-officedocument.wordprocessingml.document")}
         binding.addFileButtonPptx.setOnClickListener {
-            getFileLauncher3.launch("pptx/*")}
+            getFileLauncher3.launch("application/vnd.openxmlformats-officedocument.presentationml.presentation")}
+
+        binding.formatFileButton.setOnClickListener{
+            if(fileUri_pdf != null || fileUri_pptx != null || fileUri_word != null) {
+                findNavController().navigate(R.id.file_download_fragment)
+            }
+            else {
+                Toast.makeText(requireContext(), "Загрузите файл для обработки", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return binding.root
     }
